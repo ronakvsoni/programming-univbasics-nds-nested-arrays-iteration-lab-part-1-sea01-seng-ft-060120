0619667bb944bc513aa7d46e9fedbe548560aad9
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-26 22:39:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,8 @@
-array_of_array =[ [10, 11], [99, 50, 3, 4], [23, 41] ] 
+array_of_array =[ 
+[10, 11], 
+[99, 50, 3, 4],
+[23, 41] ] 
+
 def find_even_values(src)
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
